2ad0ce8718a7a24faeeaea62fe209b251d284983
Nicolas Stucki <nicolas.stucki@gmail.com>
2021-02-17 09:33:20 +0100

    Update to 3.0.0-RC1

--- src/main/g8/inspector/src/main/scala/inspector/Main.scala
+++ src/main/g8/inspector/src/main/scala/inspector/Main.scala
@@ -2,7 +2,7 @@ package inspector
 
 import scala.quoted._
 
-@main def run = 
-  val tastyFile = "lib/target/scala-3.0.0-M3/classes/lib/Greetings.tasty"
+@main def run =
+  val tastyFile = "lib/target/scala-3.0.0-RC1/classes/lib/Greetings.tasty"
   val tastyContents = Inspector.showCodeOf(tastyFile)
   println(tastyContents)
