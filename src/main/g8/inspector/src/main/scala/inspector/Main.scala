3cc52d35cbf43a4c331bd2309848dce7bd592b02
Anatolii Kmetiuk <anatoliykmetyuk@gmail.com>
2021-04-22 11:43:15 +0200

    Upgrade Dotty to 3.0.0-RC3

--- src/main/g8/inspector/src/main/scala/inspector/Main.scala
+++ src/main/g8/inspector/src/main/scala/inspector/Main.scala
@@ -3,6 +3,6 @@ package inspector
 import scala.quoted._
 
 @main def run =
-  val tastyFile = "lib/target/scala-3.0.0-RC2/classes/lib/Greetings.tasty"
+  val tastyFile = "lib/target/scala-3.0.0-RC3/classes/lib/Greetings.tasty"
   val tastyContents = Inspector.showCodeOf(tastyFile)
   println(tastyContents)
