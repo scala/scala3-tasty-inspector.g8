e9971c440e3c4069b32a5fa26ae511af14bf65f5
Paweł Marks <pmarks@virtuslab.com>
2024-03-29 13:45:11 +0100

    Update Scala version to 3.4.1

--- src/main/g8/inspector/src/main/scala/inspector/Main.scala
+++ src/main/g8/inspector/src/main/scala/inspector/Main.scala
@@ -3,6 +3,6 @@ package inspector
 import scala.quoted._
 
 @main def run =
-  val tastyFile = "lib/target/scala-3.4.0/classes/lib/Greetings.tasty"
+  val tastyFile = "lib/target/scala-3.4.1/classes/lib/Greetings.tasty"
   val tastyContents = Inspector.showCodeOf(tastyFile)
   println(tastyContents)
